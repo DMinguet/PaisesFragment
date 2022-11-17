7eaabe33babc57acfb7990b1711f266d4c38f359
dminguet <daminto2001@gmail.com>
2022-11-17 12:54:40 +0100

    Actualizacion confusion Kotlin 2

--- app/src/androidTest/java/com/example/paisesfragment/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/paisesfragment/ExampleInstrumentedTest.java
@@ -1,24 +1,27 @@
-package com.example.paisesfragment
+package com.example.paisesfragment;
 
-import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.ext.junit.runners.AndroidJUnit4
+import static org.junit.Assert.assertEquals;
 
-import org.junit.Test
-import org.junit.runner.RunWith
+import android.content.Context;
 
-import org.junit.Assert.*
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 /**
  * Instrumented test, which will execute on an Android device.
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-@RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
     @Test
-    fun useAppContext() {
+    public void useAppContext() {
         // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.paisesfragment", appContext.packageName)
+        Context appContext = InstrumentationRegistry.getInstrumentation().getContext();
+
+        assertEquals("com.germangascon.recyclerviewcountries", appContext.getPackageName());
     }
 }
